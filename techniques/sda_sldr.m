c5b30de440e3e752e18190d4045c00cf4eb04d04
Sajjad Karimi <50092872+sajjadkarimi91@users.noreply.github.com>
2022-07-23 17:07:37 +0430

    Update sda_sldr.m

--- techniques/sda_sldr.m
+++ techniques/sda_sldr.m
@@ -1,4 +1,4 @@
-function [para, Z] = sda_sldr(X, labels, dim, lamda_in)
+function [para, Z] = sda_sldr(X, labels, dim, lamda_in, eps_val_in)
 
 % Stochastic discriminant analysis (SDA) matches similarities between points in the projection space with those in a response space
 % Juuti, Mika, Francesco Corona, and Juha Karhunen.
@@ -17,18 +17,10 @@ function [para, Z] = sda_sldr(X, labels, dim, lamda_in)
 %    para:   output structure of sda model for input of test_sldr.m function
 %    Z:      n x dim matrix of dimensionality reduced features
 
+global dummy_labels X_org count_itr lamda
 
 classes_labels = unique(labels);
 num_classes = length(classes_labels);
-
-global dummy_labels X_org count_itr lamda
-
-count_itr=0;
-
-dummy_labels = zeros(n,num_classes);
-for k = 1:num_classes
-    dummy_labels(labels ==classes_labels(k),k) = 1;
-end
 
 if(nargin==2)
     dim= min(num_classes,max(1,size(X,2)-1));
@@ -39,6 +31,23 @@ if nargin<4
 else
     lamda = lamda_in;
 end
+
+if nargin<5
+    eps_val = 10^-5;
+else
+    eps_val = eps_val_in;
+end
+
+
+
+count_itr=0;
+
+dummy_labels = eps_val + zeros(n,num_classes);
+for k = 1:num_classes
+    dummy_labels(labels ==classes_labels(k),k) = 1;
+end
+
+
 
 % recentering original feature
 mb = mean(X,'omitnan');
@@ -75,16 +84,50 @@ para.model = 'sda';
 
 end
 
+%% cost function and its total gradient
+% For large-scale data sets, we try to write memory-efficient code instead of fast code
+
+
 function [f, g] = sda_cost(w_vec)
 
 global dummy_labels X_org count_itr lamda
+
 count_itr = count_itr+1;
 
-% w_enb = 0.000001;
-% w_dr = 0.00001;
-sn0 = 0.001;
-h_sl0 = sn0;
-h_fee = 0.00005;
+f = 0;
+f = f + lamda*sum(w_vec.^2);
+
+[K,d] = size(X_org);
+W = reshape(w_vec,d,[]);
+Z = X_org*W;
+
+P_sigma = 0;
+Q_sigma = 0;
+for i = 1:K
+
+    D2i = sum((Z(i,:)-Z).^2,2);
+    Qbi = 1./(1+D2i);
+
+    [~,ind_class] = max(dummy_labels(i,:));
+    Pi = dummy_labels(:,ind_class);
+    Q_sigma = Q_sigma + sum(Qbi);
+    P_sigma = P_sigma + sum(Pi);
+end
+
+for i = 1:K
+
+    D2i = sum((Z(i,:)-Z).^2,2);
+    Qi = (1/Q_sigma)./(1+D2i);
+    Qbi = Q_sigma*Qi;
+
+    [~,ind_class] = max(dummy_labels(i,:));
+    Pi = dummy_labels(:,ind_class)/P_sigma;
+
+    Ti = X_org(i,:) - X_org;
+
+
+end
+
 
 end
 
