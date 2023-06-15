2071aa6bef75b7d38f96ff247ed17bf135a4b3eb
Sajjad Karimi <50092872+sajjadkarimi91@users.noreply.github.com>
2023-06-15 13:20:07 +0330

    Some comments and code cleaning

--- demo_run_methods.m
+++ demo_run_methods.m
@@ -14,7 +14,7 @@ dim = 2; % dimensionality of reduced space
 similar_cov = 0; % (0->heteroscedastic), and (1->homoscedastic) covariance matrices
 separation_factor = 0.2; % (0.01<val< 0.5) Separation of classes is controlled by this parameter
 
-%% parameter init
+%% parameter initialization for data simulation
 
 for k=1:num_classes
     N(k)= round(N_init*dec_rate^k);
@@ -44,25 +44,38 @@ for k=1:num_classes
     train_label(cum_N(k)+1:cum_N(k+1))=k;
 end
 
-%% dimension reduction
+%% dimension reduction with LDA, HLDA, MMDA, WHMMDA, PLS-DA, and SDA
 
-
+disp('LDA method')
 [para_lda, Z_lda] = lda_sldr(train_data, train_label, dim); % Linear discriminant analysis (LDA)
 
+disp('HLDA method')
 [para_hlda, Z_hlda] = hlda_sldr(train_data, train_label, dim); % Heteroscedastic extension of LDA
+
 try
+    disp('MMDA method')
+    [para_mmda, Z_mmda] = mmda_sldr(train_data, train_label, dim); % Max-min distance analysis (MMDA)
+catch
+    warning('please add cvx for MMDA')
+    Z_mmda = Z_lda;
+    warning('MMDA was replaced with LDA to continue this example')
+
+end
+
+try
+    disp('WHMMDA method')
     [para_mmda, Z_mmda] = mmda_sldr(train_data, train_label, dim); % Max-min distance analysis (MMDA)
     [para_mmda, Z_whmmda] = whmmda_sldr(train_data, train_label, dim); % Heteroscedastic extension of MMDA
 catch
     warning('please add cvx for MMDA')
-    Z_mmda = Z_lda;
     Z_whmmda = Z_hlda;
-    warning('MMDA was replaced with LDA to continue this example')
     warning('WHMMDA was replaced with HLDA to continue this example')
 end
 
+disp('PLS-DA method')
 [para_plsda, Z_plsda] = plsda_sldr(train_data, train_label, dim);% Partial least squares discriminant analysis (PLS‐DA)
 
+disp('SDA method, This method is slowest method')
 [para_sda, Z_sda] = sda_sldr(train_data, train_label, dim); % Stochastic discriminant analysis (SDA)
 
 %% some EDA to analysis the results
@@ -96,7 +109,7 @@ grid on
 
 subplot(6,1,6)
 scatter(Z_plsda(:,1),Z_plsda(:,2),sz,train_label/num_classes,'filled')
-title('PLSDA')
+title('PLS-DA')
 grid on
 
 
